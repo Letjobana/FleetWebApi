c3ad317a4b0a8f467ffc65f9f8f2f0fa0dd815b6
Letjobana <suzanm@gerbertech.co.za>
2021-10-07 10:49:13 +0200

    Added Account Repository

---
+++ FleetApi/FleetApi/Repositories/Abstracts/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using FleetApi.Models;
+using System.Threading.Tasks;
+
+namespace FleetApi.Repositories.Abstracts
+{
+    public interface IAccountRepository
+    {
+        Task<Account> AddEmployee(Account account);
+    }
+}
---
+++ FleetApi/FleetApi/Repositories/Concretes/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using FleetApi.Models;
+using FleetApi.Persistance;
+using FleetApi.Repositories.Abstracts;
+using System.Threading.Tasks;
+
+namespace FleetApi.Repositories.Concretes
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+        public AccountRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<Account> AddEmployee(Account account)
+        {
+            var result = await dbContext.Accounts.AddAsync(account);
+            await dbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+    }
+}
